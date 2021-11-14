f925c8cfef79fb6591e9089f9f343290c5d04388
yunhyunchung <yhchungsera@gmail.com>
2021-11-15 04:50:02 +0900

    Piano, Maker, Category 모델 생성

--- shopping/admin.py
+++ shopping/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Piano, Maker, Category
 # Register your models here.
+
+admin.site.register(Piano)
+admin.site.register(Maker)
+admin.site.register(Category)
--- shopping/models.py
+++ shopping/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Piano(models.Model):
+    # 상품명
+    piano_name = models.CharField(max_length=30)
+    # 간단한 상품 설명
+    content = models.TextField()
+    # 상품 이미지
+
+    # 가격
+    price = models.CharField(max_length=30)
+    # 제조사 모델(외래키)
+
+    # 카테고리 모델(외래키)
+
+    def __str__(self):
+        return f'[{self.pk}]{self.piano_name}'
+
+class Maker(models.Model):
+    # 제조사명
+    maker_name = models.CharField(max_length=30)
+    # 본사 주소
+    address = models.CharField(max_length=100)
+    # 연락처
+    contact = models.CharField(max_length=30)
+    # 인터넷 사이트 주소
+    ip_address  = models.CharField(max_length=100)
+
+class Category(models.Model):
+    # 건반타입
+    keytype = models.CharField(max_length=20)
+    # 벨로시티(건반 터치 감도)
+    touch_response = models.CharField(max_length=10)
+    # 폴리포니
+    poly_phony = models.CharField(max_length=10)
